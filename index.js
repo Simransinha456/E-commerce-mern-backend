129533ec1b79ed2749e2622c63b4abf7c93b622d
Simran Sinha <simransinha280@gmail.com>
2024-03-17 00:41:55 +0530

    Refactor image upload functionality

--- index.js
+++ index.js
@@ -33,26 +33,26 @@ app.get("/", (req, res) => {
 });
 
 //Image storage engine
-// const storage = multer.diskStorage({
-//   destination: "./upload/images",
-//   filename: (req, file, cb) => {
-//     return cb(
-//       null,
-//       `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
-//     );
-//   },
-// });
-// //using multer we create one upload folder
-// const upload = multer({ storage: storage });
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+//using multer we create one upload folder
+const upload = multer({ storage: storage });
 
-//creating upload endpoint for images
-// app.use("/images", express.static("upload/images"));
-// app.post("/upload", upload.single("product"), (req, res) => {
-//   res.json({
-//     success: 1,
-//     image_url: `http://localhost:${8000}/images/${req.file.filename}`,
-//   });
-// });
+// creating upload endpoint for images
+app.use("/images", express.static("upload/images"));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${8000}/images/${req.file.filename}`,
+  });
+});
 
 //Routing page of productRoute and userRoute
 app.use("/newproduct", productRoute);
